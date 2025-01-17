e388c4c04a04b65c15e6db7f2dc2480d385526e9
Cihan <cihankan53@gmail.com>
2025-01-17 19:26:29 +0300

    AddedPostgres

--- main.go
+++ main.go
@@ -1,25 +1,51 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Todo struct {
-	ID   string `json:"id"`
+	ID   uint   `json:"id" gorm:"primaryKey;autoIncrement"`
 	Task string `json:"task"`
-	Done bool   `json:"done"`
+	Done bool   `json:"done" gorm:"default:false"`
 }
 
 func main() {
 	app := fiber.New()
+	db, err := gorm.Open(postgres.Open("host=localhost user=postgres password=123456 dbname=postgres port=5432 sslmode=disable"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&Todo{})
+
 	app.Post("/user", func(c *fiber.Ctx) error {
-		var Todo Todo
-		if err := c.BodyParser(&Todo); err != nil {
+		var todo Todo
+		if err := c.BodyParser(&todo); err != nil {
 			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 				"error": "Invalid input",
 			})
 		}
-		return c.JSON(Todo)
+		if todo.Task == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Task is required",
+			})
+		}
+		fmt.Println(todo)
+		if result := db.Create(&todo); result.Error != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": result.Error.Error(),
+			})
+		}
+		return c.JSON(fiber.Map{
+			"id":      todo.ID,
+			"task":    todo.Task,
+			"done":    todo.Done,
+			"message": "Todo created successfully",
+		})
 	})
 
 	app.Listen(":3000")
